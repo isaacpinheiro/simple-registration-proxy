9f1421810f9feb3f0fb3cdce799353a6f504e2a0
Isaac Pinheiro <isaacpnhr@gmail.com>
2022-03-27 18:16:13 -0300

    Mudando dev_eui, access_token e app_id para unique.

--- db.sql
+++ db.sql
@@ -3,9 +3,9 @@ use simple_registration_proxy;
 
 create table device(
     id serial,
-    dev_eui varchar(255) not null,
-    access_token varchar(255) not null,
-    app_id varchar(255) not null,
+    dev_eui varchar(255) not null unique,
+    access_token varchar(255) not null unique,
+    app_id varchar(255) not null unique,
     primary key(id)
 );
 
